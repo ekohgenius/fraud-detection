a4dd62018b55c69e708120d9effdb9b1e0ecd40b
Courage Ekoh <54238862+ekohgenius@users.noreply.github.com>
2022-03-03 13:44:05 +0200

    minor correction

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 
-r = requests.post('https://courage_fraud_detection.herokuapp.com/is-fraud', json = {
-"step":1,
-"type":"PAYMENT",
-"amount":9839.64,
-"nameOrig":"C1231006815",
-"oldbalanceOrig":170136.0,
-"newbalanceOrig":160296.36,
-"nameDest":"M1979787155",
-"oldbalanceDest":0.0,
-"newbalanceDest":0.0
-})
+r = requests.post('https://courage-fraud-detection.herokuapp.com/is-fraud', json = {
+        "step":1,
+        "type":"PAYMENT",
+        "amount":9839.64,
+        "nameOrig":"C1231006815",
+        "oldbalanceOrig":170136.0,
+        "newbalanceOrig":160296.36,
+        "nameDest":"M1979787155",
+        "oldbalanceDest":0.0,
+        "newbalanceDest":0.0
+    })
 
 print(r.text)
